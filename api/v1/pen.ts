b3e6ebafb4b79cb87f5e6de94e6120e6b61dba8c
Emile Aublet <emileaublet@gmail.com>
2025-09-17 10:16:36 -0400

    so

--- api/v1/pen.ts
+++ api/v1/pen.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import pens from "../../src/pens/pens.json";
+import pens from "../../src/pens/pens.json" assert { type: "json" };
 
 export default function handler(_req: VercelRequest, res: VercelResponse) {
   res.status(200).json(pens);
